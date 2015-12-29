70e2362fab2a0a0ca1ac89030feb2aa645b0b8d9
Andrew Eckford <aeckford@gmail.com>
2015-12-29 10:00:22 -0500

    Added a python version of the function

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SumOfTwoExponentialEntropy
 MATLAB version of the function: h_2_exponential.m
+Python version of the function: h_2_exponential.py (requires scipy)
---
+++ h_2_exponential.py
@@ -0,0 +1,34 @@
+import scipy as sp
+import scipy.special as sps
+
+def h_2_exponential(lx,lw):
+  # h_2_exponential(lx,lw)
+  # 
+  # Calculates the entropy of y = x + w, where x and w are 
+  # two independent, arbitrarily-distributed exponential random variables.
+  #
+  # Requires scipy.
+  #
+  # lx and lw are the (lambda) parameters of x and w, respectively
+  #
+  # lx and lw can be either scalars or scipy arrays, but they must have
+  # the same dimensions
+
+
+  foo = sp.sort(sp.vstack((lx,lw)),0)
+
+  hy = [h_noteq(r[0],r[1]) if (r[0]!=r[1]) else h_eq(r[0]) for r in sp.transpose(foo)]
+
+  return sp.array(hy)
+
+def h_eq(lw):
+  # calculates entropy when the parameters are equal
+  return (2 - sp.special.psi(2) - sp.log(lw));
+
+def h_noteq(lx,lw):
+  # calculates entropy when the parameters are nonequal
+  eg = sp.euler_gamma
+  hy = (1+eg+sps.psi(1+lx/(lw-lx))-sp.log(lw*lx/(lw-lx)))/((lw-lx)/lw)
+  hy = hy - ((lx/lw)+eg+sps.psi(1+lw/(lw-lx))-sp.log(lw*lx/(lw-lx)))/((lw-lx)/lx)
+  return hy
+
